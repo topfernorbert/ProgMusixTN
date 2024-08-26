15772e60a98f6b20ba8762a594ab89b9931230d1
topfernorbert <norbert.topfer@gmail.com>
2024-08-26 13:43:42 +0200

    proba1

--- configs/config.py
+++ configs/config.py
@@ -4,10 +4,10 @@ from selenium.webdriver.chrome.options import Options
 def get_preconfigured_chrome_driver() -> webdriver.Chrome:
     options = Options()
     options.add_experimental_option('detach', True)
-    options.add_argument('--headless')
-    options.add_argument("--start-maximized")
+    # options.add_argument('--headless')
+    # options.add_argument("--start-maximized")
     options.add_argument("--disable-search-engine-choice-screen")
-    #options.add_argument('window-position=2000,50')
+    options.add_argument('window-position=2000,50')
     # options.add_argument("--lang=hu")
     options.add_argument("--lang=en")
     # options.add_experimental_option('excludeSwitches', ['enable-logging'])  # Allure certificate error esetén
