2bce9487f67c4252fee756d648779a807d6701bd
topfernorbert <norbert.topfer@gmail.com>
2024-08-26 13:36:54 +0200

    proba1

--- configs/config.py
+++ configs/config.py
@@ -5,6 +5,7 @@ def get_preconfigured_chrome_driver() -> webdriver.Chrome:
     options = Options()
     options.add_experimental_option('detach', True)
     options.add_argument('--headless')
+    options.add_argument("--start-maximized")
     options.add_argument("--disable-search-engine-choice-screen")
     #options.add_argument('window-position=2000,50')
     # options.add_argument("--lang=hu")
