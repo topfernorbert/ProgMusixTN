f7e8656a099d3662157bc351be2e99b4ec763abb
topfernorbert <norbert.topfer@gmail.com>
2024-08-29 13:23:08 +0200

    proba1

--- test_progmusix.py
+++ test_progmusix.py
@@ -43,14 +43,15 @@ class TestProgmusix:
         self.page.contact_field().send_keys(TESTDATA['contact_field'])
         self.page.contact_send_btm().click()
         #Egyezik, megjelenik az adatbázisban a beírt név, email cím, üzenet szövege:
-        #assert TESTDATA['contact_name'] and TESTDATA['contact_email'] and TESTDATA['contact_field'] in self.page.last_msg()
+        assert TESTDATA['contact_name'] and TESTDATA['contact_email'] and TESTDATA['contact_field'] in self.page.last_msg()
 
-    def test_categories(self):
-        url = "http://localhost:8080/api/categories"
-        payload = {}
-        headers = {}
-        response = requests.request("GET", url, headers=headers, data=payload)
-        assert response.text == TESTDATA['Postman_categories']
+    # def test_categories(self):
+    #     url = "http://localhost:8080/api/categories"
+    #     payload = {}
+    #     headers = {}
+    #     response = requests.request("GET", url, headers=headers, data=payload)
+    #     assert response.text == TESTDATA['Postman_categories']
+    #     print(response.text)
 
 
 
