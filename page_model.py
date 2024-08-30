f4739875f4a6469a7fdc85c828c2f2a90d17996c
topfernorbert <norbert.topfer@gmail.com>
2024-08-30 10:54:36 +0200

    proba1

--- page_model.py
+++ page_model.py
@@ -184,7 +184,7 @@ class ProgMusix(GeneralPage):
 
     def last_msg(self):
         connection = mysql.connector.connect(
-            host="localhost",
+            host="mysql-container",
             port=3306,
             user="root",
             password="test1234",
