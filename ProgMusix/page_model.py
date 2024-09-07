e11cd5368352cf9cf50ab5e2235a42d6cf2e9e2a
topfernorbert <161230258+topfernorbert@users.noreply.github.com>
2024-09-07 09:23:17 +0200

    Update page_model.py

--- ProgMusix/page_model.py
+++ ProgMusix/page_model.py
@@ -392,11 +392,7 @@ class ProgMusix(GeneralPage):
         # self.login_method_p()
         self.menu_cart().click()
         self.checkout_btn().click()
-        time.sleep(2)
-        self.btn_step2().click()
-        time.sleep(2)
         self.input_username().send_keys(TESTDATA['username_p'])
-
         #self.input_email().send_keys(TESTDATA['email_p'])
         self.input_phonenumber().send_keys(TESTDATA['phone_p'])
         self.btn_step2().click()
