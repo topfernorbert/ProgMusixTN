46dac1766ba9a2e3cee1336183d5fa79a85981e2
topfernorbert <161230258+topfernorbert@users.noreply.github.com>
2024-09-08 10:18:17 +0200

    Update page_model.py

--- ProgMusix/page_model.py
+++ ProgMusix/page_model.py
@@ -254,8 +254,7 @@ class ProgMusix(GeneralPage):
 
     def registration_method(self):
         self.menu_login_btn().click()
-        time.sleep(3)
-        self.create_page_btn().click()
+        self.btn_createaccount().click()
         self.email().send_keys(TESTDATA['email_p'])
         self.username().send_keys(TESTDATA['username_p'])
         self.password().send_keys(TESTDATA['password_p'])
