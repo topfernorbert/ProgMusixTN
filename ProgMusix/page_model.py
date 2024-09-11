d21ec2af979a6edac32846463c301e10bb27efd3
topfernorbert <161230258+topfernorbert@users.noreply.github.com>
2024-09-11 10:42:28 +0200

    Update page_model.py

--- ProgMusix/page_model.py
+++ ProgMusix/page_model.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import ElementClickInterceptedException
 from random import randint
 import requests
 
