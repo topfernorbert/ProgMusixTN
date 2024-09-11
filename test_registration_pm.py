07ccd73e8cb5d38333599e5bd103c8b61d06ba5e
topfernorbert <norbert.topfer@gmail.com>
2024-09-11 11:59:09 +0200

    Update

--- test_registration_pm.py
+++ test_registration_pm.py
@@ -3,11 +3,15 @@ from ProgMusix.page_model import ProgMusix
 from ProgMusix.configs.testdata import TESTDATA
 import pytest
 
-
-def test_regtest(self):
-    self.page.registration()
-    assert self.page.current_url() == "http://localhost:4200/registration"
-    self.page.reg_user_data_send()
-    assert self.page.register_btn().is_enabled()
-    assert self.page.succesfull_msg() == TESTDATA['succesful_msg']
-    assert TESTDATA['email_p'] and TESTDATA['username_p'] in self.page.last_user_sql()
+class TestProgmusix:
+    def setup_method(self):
+        page = ProgMusix(get_preconfigured_chrome_driver())
+        self.page = page
+        page.open()
+    def test_regtest(self):
+        self.page.registration()
+        assert self.page.current_url() == "http://localhost:4200/registration"
+        self.page.reg_user_data_send()
+        assert self.page.register_btn().is_enabled()
+        assert self.page.succesfull_msg() == TESTDATA['succesful_msg']
+        assert TESTDATA['email_p'] and TESTDATA['username_p'] in self.page.last_user_sql()
